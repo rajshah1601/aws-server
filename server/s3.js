1a18614988f5f655d406758a36a9b9e5b793d443
rajshah1606 <raj.shah@budhanatech.com>
2023-03-30 15:48:36 +0530

    first

---
+++ server/s3.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+const fs = require('fs')
+const S3 = require("aws-sdk/clients/s3")
+const sharp = require('sharp')
+
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+//Uploads File to s3
+function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path)
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+    return s3.upload(uploadParams).promise()
+}
+exports.uploadFile = uploadFile
+
+function getThumbnails(fileKey){
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: "upload-file-s3-demo/thumbnails"
+    }
+    const result = s3.getObject(downloadParams).createReadStream()
+    return result
+}
+exports.getThumbnails = getThumbnails
+//Downloads a file from s3
+function getFileStream(fileKey) {
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName
+    }
+    return s3.getObject(downloadParams).createReadStream()
+}
+exports.getFileStream = getFileStream
+exports.handler = async (event) => {
+    const bucketName = event.Records[0].s3.bucket.bucketName
+    const objectKey = event.Records[0].s3.object.key;
+    const inputObject = await s3.getObject({ Bucket: bucketName, Key: objectKey }).promise();
+
+    const thumbnail = await sharp(inputObject.Body).resize(200, 200).toBuffer();
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: thumbnail,
+        Key: `thumbnails/${objectKey}`
+    };      
+
+    await s3.upload(uploadParams).promise();
+
+    return `Thumbnail generated for ${objectKey}`;
+}
---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require("express")
+
+const fs = require('fs')
+const utils = require('util')
+const unlinkFile = utils.promisify(fs.unlink)
+
+
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
+const app = express()
+const { uploadFile, getFileStream, handler, getThumbnails } = require("./s3")
+
+app.get("/images/:key", (req, res) => {
+    const key = req.params.key
+    const readStream = getFileStream(key)
+    readStream.pipe(res)
+})
+
+app.get("/images/thumbnails/:key", (req, res) => {
+    const key = req.params.key
+    const readStream = getThumbnails(key)
+    readStream.pipe(res)
+})
+
+app.post("/images", upload.single('image'), async (req, res) => {
+    const file = req.file
+    const result = await uploadFile(file)
+    await unlinkFile(file.path)
+    const thumbnailKey = `thumbnails/${result.key}`;
+    await handler({
+        Records: [{
+            s3: {
+                bucket: {
+                    bucketName: result.Bucket
+                },
+                object: {
+                    key: result.Key
+                }
+            }
+        }]
+    });
+    const description = req.body.description
+    res.send({
+        imagePath: `/images/${result.key}`,
+        thumbnailPath: `/images/${thumbnailKey}`
+    });
+})
+
+app.listen(8000, () => {
+    console.log("Listening on Port 8000")
+})
